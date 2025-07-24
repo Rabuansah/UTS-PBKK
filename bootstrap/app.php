b62b5f35c006a2a0df5cf90964b30bf8af8971c8
Rabuasnah <rabuansah21@gmail.com>
2025-07-25 01:55:09 +0700

    CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,20 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'stripe/*',
+            'http://localhost:3000/login',
+            'http://127.0.0.1:8000/api/login',
+            'http://127.0.0.1:8000/api/*',
+            'http:///foo/bar',
+            'http://example.com/foo/*',
+        ]);
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
